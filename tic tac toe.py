4ceaa89b35d26c07a4216474444e0da65d930499
Romish Yadav <140260912+romish-yadav@users.noreply.github.com>
2025-02-10 18:22:23 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -53,7 +53,7 @@ def CheckWin():
     else:            
         Game=Running    
     
-print("Tic-Tac-Toe Game Designed By Sourabh Somani")    
+print("Tic-Tac-Toe Game Designed By Romish Yadav")    
 print("Player 1 [X] --- Player 2 [O]\n")    
 print()    
 print()    
